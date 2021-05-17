12dde76db40be22f95c90c66a2ea4bfb6b405cd6
Tierra Rogers <tierragore@yahoo.com>
2021-05-16 21:57:12 -0400

    Updating files

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,30 @@
--- Drops the employeeTracker_db if it exists currently --
-DROP DATABASE IF EXISTS employeeTracker_db;
--- Creates the "employeeTracker_db" database --
-CREATE DATABASE employeeTracker_db;
-
--- Makes it so all of the following code will affect employeeTracker_db --
-USE employeeTracker_db;
-
--- Creates the table "department" within eployeeTracker_db --
-CREATE TABLE department(
-    id int NOT NULL AUTO_INCREMENT,
-  -- Makes a string column called "NAME" 
-  name VARCHAR(30),
-  PRIMARY KEY (id)
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+​
+USE employees;
+​
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
-
+​
 CREATE TABLE role (
-    id int NOT NULL AUTO-AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL NOT NULL,
-    department_id   INT NOT NULL,
-    PRIMARY KEY (id)
-  
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
-
+​
 CREATE TABLE employee (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30;)
-last_name VARCHAR(30);
-    role_id INT NOT NULL,
-    isManager= BOOLEAN NOT NULL;
-superviserORManager_Id INT,
-PRIMARY KEY (Id)
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
